5c27718533549bb860b55e524c43cb8f8ea4788c
Aflah Sedhique <aflah3100@gmail.com>
2024-10-11 10:21:40 +0530

    Updated detail screen

--- lib/screens/detail_screen/detail_screen.dart
+++ lib/screens/detail_screen/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon_explorer_app/models/webtoon_model.dart';
+import 'package:webtoon_explorer_app/utils/app_colors.dart';
 import 'package:webtoon_explorer_app/utils/app_fonts.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -12,29 +13,52 @@ class DetailScreen extends StatelessWidget {
     final height = MediaQuery.sizeOf(context).height;
     final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             expandedHeight: height * 0.40,
-            toolbarHeight: 40,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(
-                webtoon.image ?? "",
-                fit: BoxFit.contain,
-              ),
+            toolbarHeight: 45,
+            pinned: true,
+            title: const Text(''),
+            flexibleSpace: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                var percentage = (constraints.maxHeight - kToolbarHeight) /
+                    (height * 0.40 - kToolbarHeight);
+                bool isCollapsed = percentage < 0.2;
+
+                return FlexibleSpaceBar(
+                  title: isCollapsed
+                      ? Text(
+                          webtoon.title ?? 'Webtoon',
+                          style: AppFonts.poppinsTextStyle(
+                            fontSize: 18,
+                            fontColor: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : null,
+                  centerTitle: true,
+                  background: Image.network(
+                    webtoon.image ?? "",
+                    fit: BoxFit.fill,
+                  ),
+                  collapseMode: CollapseMode.pin,
+                );
+              },
             ),
           ),
           SliverToBoxAdapter(
             child: Container(
               padding: const EdgeInsets.all(15.0),
               width: width,
-              height: 400,
               decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(36),
-                      topRight: Radius.circular(36))),
+                color: AppColors.appYellowColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -44,55 +68,55 @@ class DetailScreen extends StatelessWidget {
                       Flexible(
                         child: Text(webtoon.title ?? 'Webtoon',
                             maxLines: 2,
-                            overflow: TextOverflow.clip,
+                            overflow: TextOverflow.ellipsis,
                             textAlign: TextAlign.start,
                             style: AppFonts.poppinsTextStyle(
                                 fontSize: 25,
                                 fontColor: Colors.black,
                                 fontWeight: FontWeight.bold)),
                       ),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.favorite))
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite),
+                      ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 5,
-                  ),
+                  const SizedBox(height: 5),
                   Text(
-                    maxLines: 1,
-                    overflow: TextOverflow.fade,
                     "Creator: ${webtoon.creator ?? "Not found"}",
                     style: AppFonts.poppinsTextStyle(
                         fontSize: 18,
                         fontColor: Colors.grey,
                         fontWeight: FontWeight.w500),
                   ),
-                  const SizedBox(
-                    height: 2,
-                  ),
+                  const SizedBox(height: 2),
                   Text(
-                    maxLines: 1,
-                    overflow: TextOverflow.fade,
                     "Genre: ${webtoon.genre ?? "Not found"}",
                     style: AppFonts.poppinsTextStyle(
                         fontSize: 18,
                         fontColor: Colors.grey,
                         fontWeight: FontWeight.w500),
                   ),
-                  const SizedBox(
-                    height: 10,
+                  const SizedBox(height: 2),
+                  Text(
+                    "Rating: ",
+                    style: AppFonts.poppinsTextStyle(
+                        fontSize: 18,
+                        fontColor: Colors.grey,
+                        fontWeight: FontWeight.w500),
                   ),
-                  Flexible(
-                      child: Text(
-                    webtoon.description ?? "",
+                  const SizedBox(height: 10),
+                  Text(
+                    " ${webtoon.description}\n${webtoon.description}\n${webtoon.description}",
                     style: AppFonts.poppinsTextStyle(
                         fontSize: 15,
                         fontColor: Colors.black,
                         fontWeight: FontWeight.w400),
-                  ))
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appYellowColor = Color(0xfffff9ef);
+}
